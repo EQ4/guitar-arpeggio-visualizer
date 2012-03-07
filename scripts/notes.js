96efd42144617db6420220bd2303b8f5b6858bbd
Ethan Gardner <edgincvg@gmail.com>
2012-03-06 22:45:25 -0500

    added comments to javascript file

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,7 +1,21 @@
+/**
+ * @author Ethan Gardner
+ * 
+ */
+
 var paper = new Raphael(document.id("fretboard"), 1300, 300);
 
+/**
+ * Set up a GTR object to be used for namespacing 
+ * @see http://www.ethangardner.com/articles/javascript-namespace-strategy-for-large-applications/
+ * for more information.
+ */  
 var GTR = {};
 
+/**
+ * An array of all the notes on a guitar with 24 frets.
+ * There is one array per string.
+ */ 
 GTR.strings = [
 	['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#',  'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E'],
 	['B', 'C', 'C#', 'D', 'D#',  'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#',  'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
@@ -11,6 +25,10 @@ GTR.strings = [
 	['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#',  'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
 ];
 
+/**
+ * Calculates the position for each string and draws 
+ * the string on the canvas 
+ */
 GTR.drawStrings = function(){
 	var numStrings = GTR.strings.length;
 		currentString = i + 1;
@@ -20,6 +38,10 @@ GTR.drawStrings = function(){
 	}
 }
 
+/**
+ * Similar to the drawStrings function, this calculates the position 
+ * for each fret and draws the string on the canvas 
+ */
 GTR.drawFrets = function(){
 	for(i = 0; i < 24; i++){
 		var currentFret = i + 1;
@@ -27,6 +49,9 @@ GTR.drawFrets = function(){
 	}
 }
 
+/**
+ * Puts the position markers on the neck at the specified location
+ */
 GTR.drawMarkers = function(){
 	paper.set(
 		paper.circle(175, 147, 10, 10),
@@ -44,6 +69,10 @@ GTR.drawMarkers = function(){
 	).attr({fill: "#d00"});
 }
 
+/**
+ * Draws all the components of the guitar in the 
+ * order needed to 
+ */
 GTR.buildGuitar = function(){
 	paper.rect(50, 10, 1200, 275).attr({fill: "#87600C"}); // fretboard
 	
@@ -57,26 +86,31 @@ window.addEvent('load', function(){
 	GTR.buildGuitar();
 	
 	var numStrings = GTR.strings.length,
-		st = paper.set();
+		st = paper.set(); // add the notes from user input to a set so they can be removed easily
 
 	/**
-	 * Place Markers
+	 * Diagram the notes based on user input
 	 */
 	$('draw').addEvent('click', function(){
 		var i = 0,
 			markers = [],
 	    	checked = $$('input[name=notes]:checked');
-	    	
-		st.remove();
+	    
+		st.remove(); // remove the notes in the set
 	    
 	    if(checked.length !== 0){
 			checked.map(function(el){
-				markers.push(el.value + '(?!#)');
+				markers.push(el.value + '(?!#)'); // loop over all checked notes and push into an array
 		    });
 
-			markers = markers.join("|");
+			markers = markers.join("|"); // convert array to string for use in regex
 			markers = new RegExp(markers);
-
+			
+			/**
+			 * Loop through all strings and then loop through notes
+			 * on each string. If a match to the regex is detected,
+			 * plot the note on the guitar for each string.
+			 */
 		    GTR.strings.each(function(item, index){
 			    item.each(function(item, index){
 				    if(markers.test(item)){
@@ -91,6 +125,10 @@ window.addEvent('load', function(){
 	    } 
 	});
 	
+	/**
+	 * Give the user an easy way to clear all notes
+	 * and uncheck all boxes.
+	 */
 	$('reset').addEvent('click', function(){
 		st.remove();
 		$$('input[name=notes]:checked').set('checked', false);
